77a06ebb44678f5edcf88fe58749486048969504
Sayed <nuruddinsayeed@gmail.com>
2021-05-10 05:53:04 +0600

    ebook app with database added

---
+++ ebooks_api/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Ebook(models.Model):
+    """Create ebook data model"""
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    """Create Books Review data model"""
+
+    ebook = models.ForeignKey(
+        Ebook,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    review_author = models.CharField(max_length=8, null=True, blank=True)
+    review = models.TextField(blank=True, null=True)
+    rating = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
+    def __str__(self):
+        return self.rating
